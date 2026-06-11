f197f07c4905f90803b55fc6849784fd021d3485
rafiq_sheriff <sherifffrafiq71@gmail.com>
2026-06-11 23:53:34 +0530

    feat:fixed the config, works

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Use a relative base so assets load correctly when served from Vercel
 export default defineConfig({
-	base: './',
-	plugins: [react()],
+  plugins: [react()],
+  base: '/'
 })
